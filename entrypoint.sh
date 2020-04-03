34d542e0430ef30d39381588f81ded0dcf68fcf6
Andreas Karis <akaris@redhat.com>
2020-04-03 13:16:34 -0400

    new commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,11 @@ mv dnsmasq.conf /etc/dnsmasq.conf
 # run dnsmasq
 /usr/sbin/dnsmasq -k --log-facility=/dev/stdout &
 
+# run httpd
+# making my life easier - just link this instead of reconfiguring httpd
+rmdir /var/www/html
+ln -s /httpboot /var/www/html
+/usr/sbin/httpd -DFOREGROUND &
+
 # sleep for all eternity
 sleep infinity
