a407d3a6c9b912fbb13106488f21178e899c54d3
Andreas Karis <ak.karis@gmail.com>
2020-06-16 18:08:24 +0200

    correction

--- example_netinstall.sh
+++ example_netinstall.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 echo "Installing Fedora PXE boot image for debugging"
-curl -L -o /tmp/fedora.iso "https://download.fedoraproject.org/pub/fedora/linux/releases/32/Server/x86_64/iso/Fedora-Server-netinst-x86_64-32-1.6.iso"
-mkdir /mnt/fedora
-mount -o loop /tmp/fedora.iso /mnt/fedora/
-mkdir /httpboot/fedora
-\cp /mnt/fedora/images/pxeboot/* /httpboot/fedora/.
+curl -L -o /tmp/fedora.iso "https://download.fedoraproject.org/pub/fedora/linux/releases/32/Server/x86_64/iso/Fedora-Server-dvd-x86_64-32-1.6.iso"
+mkdir /httpboot/fedora/dvd -p
+mount -o loop /tmp/fedora.iso /httpboot/fedora/dvd/
+\cp /httpboot/fedora/dvd/images/pxeboot/* /httpboot/fedora/.
 umount /mnt/fedora
 cat << 'EOF' > /httpboot/fedora/boot-menu-entry 
 set server_root http://{{ PXE_LISTEN_ADDRESS }}/fedora/
@@ -13,12 +12,12 @@ initrd ${server_root}/initrd.img
 kernel ${server_root}/vmlinuz ip=dhcp ipv6.disable initrd=initrd.img inst.ks=${server_root}/kickstart.cfg
 boot
 EOF
-cat << 'EOF' > /httpboot/fedora/kickstart.cfg
+source config 
+cat << EOF > /httpboot/fedora/kickstart.cfg
+url --url http://${PXE_LISTEN_ADDRESS }/fedora/dvd/
 network --bootproto=dhcp --device=bootif
 rootpw --plaintext password
 reboot
-repo --name=fedora
-repo --name=updates
 firstboot --disable
 services --enabled="sshd,chronyd"
 zerombr
